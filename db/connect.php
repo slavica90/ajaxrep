5bfc7cf8c20403653061689504ccb9a2ee66fbc4
slavica <>
2013-11-06 13:33:24 +0100

    Aaaa

---
+++ db/connect.php
@@ -0,0 +1,3 @@
+<?php
+mysql_connect('localhost','root','mignix4');
+mysql_select_db(ajaxdb);
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>AJAX Database</title>
+  </head>
+  <body>
+    Name: <input type="text" id="name">
+    <input type="submit" id="name-submit" value="Prikazi">
+    <div id="name-data"></div>
+    <script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="js/global.js"></script>
+    <?php
+    // put your code here
+    ?>
+  </body>
+</html>
